818d75ff5df2f45ae00912a9304e561be81cbb06
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-20 02:47:10 -0800

    distort dots

--- src/js/dot.js
+++ src/js/dot.js
@@ -1,7 +1,8 @@
-OctopusSkin.dot = function(svg) {
+(function() {
 
   var radius = OctopusSkin.width / 10;
   var segments = 12;
+  var distortionRange = radius / 4;
 
   var controlShortSide = (4 / 3) * Math.tan(Math.PI / (2 * segments)) * radius;
   var controlRadius = Math.pow(Math.pow(controlShortSide, 2) + Math.pow(radius, 2), 0.5);
@@ -9,32 +10,60 @@ OctopusSkin.dot = function(svg) {
 
   var segmentAngle = (2 * Math.PI) / segments;
 
-  var points = [];
-  for (var i = 0; i < segments; i += 1) {
-    var pointAngle = segmentAngle * i;
-    var point = OctopusSkin.polarToCart(radius, pointAngle);
+  window.OctopusSkin.dot = function(svg) {
 
-    point.ca = OctopusSkin.polarToCart(controlRadius, pointAngle + controlAngle);
-    point.cb = OctopusSkin.polarToCart(controlRadius, pointAngle + segmentAngle - controlAngle);
-    
-    points.push(point);
-  }
+    var points = [];
+    for (var i = 0; i < segments; i += 1) {
+      var pointAngle = segmentAngle * i;
+      var dv = distortionVector();
 
-  var path = 'M ' + points[0].x + ' ' + points[0].y;
-  for (var i = 0; i < segments; i += 1) {
-    var point = points[i];
-    var nextPoint = points[i + 1];
-    if (typeof nextPoint === 'undefined') {
-      nextPoint = points[0];
+      var point = sumPoints(polarToCart(radius, pointAngle), dv);
+
+      point.cb = sumPoints(polarToCart(controlRadius, pointAngle - controlAngle), dv);
+      point.ca = sumPoints(polarToCart(controlRadius, pointAngle + controlAngle), dv);
+      
+      points.push(point);
     }
-    path += ' C ' +
-        point.ca.x + ' ' + point.ca.y + ', ' +
-        point.cb.x + ' ' + point.cb.y + ', ' +
-        nextPoint.x + ' ' + nextPoint.y;
+
+    var path = 'M ' + points[0].x + ' ' + points[0].y;
+    for (var i = 0; i < segments; i += 1) {
+      var point = points[i];
+      var nextPoint = points[i + 1];
+      if (typeof nextPoint === 'undefined') {
+        nextPoint = points[0];
+      }
+      path += ' C ' +
+          point.ca.x + ' ' + point.ca.y + ', ' +
+          nextPoint.cb.x + ' ' + nextPoint.cb.y + ', ' +
+          nextPoint.x + ' ' + nextPoint.y;
+    }
+
+    return svg.append('path')
+        .attr('stroke', 'black')
+        .attr('fill', 'transparent')
+        .attr('d', path);
+  };
+
+  function polarToCart(distance, angle) {
+    var x = Math.cos(angle) * distance;
+    var y = Math.sin(angle) * distance;
+    return {
+      x: x,
+      y: y
+    };
   }
 
-  return svg.append('path')
-      .attr('stroke', 'black')
-      .attr('fill', 'transparent')
-      .attr('d', path);
-};
+  function distortionVector() {
+    return {
+      x: (Math.random() * distortionRange) - (distortionRange / 2),
+      y: (Math.random() * distortionRange) - (distortionRange / 2)
+    };
+  }
+
+  function sumPoints(a, b) {
+    return {
+      x: a.x + b.x,
+      y: a.y + b.y
+    };
+  };
+})();
--- src/js/octopus_skin.js
+++ src/js/octopus_skin.js
@@ -1,12 +1,4 @@
 window.OctopusSkin = {
   height: 700,
-  width: 700,
-  polarToCart: function(distance, angle) {
-    var x = Math.cos(angle) * distance;
-    var y = Math.sin(angle) * distance;
-    return {
-      x: x,
-      y: y
-    };
-  }
+  width: 700
 };
