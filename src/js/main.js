28aacb6b533bda036c6c2539b71560df01771ba1
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-20 00:29:35 -0800

    make red dot

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,21 @@
-console.log('why hello good sir!');
+window.OctopusSkin = Object.create(null);
+
+(function() {
+
+  var height = 700;
+  var width = height;
+
+  var svg = d3.select('.svg')
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height)
+    .append("g")
+      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+  svg.append('circle')
+      .attr('cx', 0)
+      .attr('cy', 0)
+      .attr('r', 200)
+      .attr('fill', 'red');
+
+})();
