3786ed58df6fc2c9280c53e3037627eb74f30522
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-20 19:26:52 -0800

    move some variables around

--- src/js/octopus_skin.js
+++ src/js/octopus_skin.js
@@ -1,4 +1,8 @@
-window.OctopusSkin = {
-  height: 700,
-  width: 700
-};
+(function() {
+  window.OctopusSkin = {
+    height: 700,
+    width: 700,
+    spotRadius: 30,
+    minSpotDistance: 60
+  };
+})();
