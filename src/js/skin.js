7b9bae779ed5f050b8853172627f5342c0881eec
Andrew Short <andrewshort@Andrews-Mac-mini.local>
2016-02-26 02:17:09 -0800

    remove color variable

--- src/js/skin.js
+++ src/js/skin.js
@@ -4,8 +4,6 @@
 
   function skin(svg) {
 
-    var color = OctopusSkin.color();
-
     var el = svg.append('rect')
         .attr('x', 0 - (OctopusSkin.width / 2))
         .attr('y', 0 - (OctopusSkin.height / 2))
@@ -15,7 +13,6 @@
 
     return({
       el: el,
-      color: color,
       setColor: setColor.bind(null, el)
     });
   };
